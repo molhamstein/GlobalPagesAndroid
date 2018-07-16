43f11c29e0db587a15aa3845263dc88e7acbed66
Adhamkh <adhamkhwaldeh@gmail.com>
2018-07-16 18:39:05 +0300

    First submit

--- app/src/main/java/com/brainsocket/globalpages/activities/BaseActivity.kt
+++ app/src/main/java/com/brainsocket/globalpages/activities/BaseActivity.kt
@@ -3,12 +3,17 @@ package com.brainsocket.globalpages.activities
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 /**
  * Created by Adhamkh on 2018-06-08.
  */
 abstract class BaseActivity : AppCompatActivity() {
+
+    init {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+    }
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
